decb32e02eb3bf2d7f1852921e5ea10584d15791
이우성 <42796944+leewooseong@users.noreply.github.com>
2024-07-15 00:40:41 +0900

    [Bronze IV] Title: $A + B - C$, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+
+        int integerA = Integer.parseInt(a);
+        int integerB = Integer.parseInt(b);
+        int integerC = Integer.parseInt(c);
+
+        System.out.println(integerA + integerB - integerC);
+        System.out.println(Integer.parseInt(a + b) - integerC);
+    }
+}
