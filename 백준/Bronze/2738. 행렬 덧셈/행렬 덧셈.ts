f91de579f092de7a5ce39f5b5f174bd26950ac96
이우성 <42796944+leewooseong@users.noreply.github.com>
2024-08-05 19:39:09 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 100 ms, Memory: 10104 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.ts
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : "src/Baekjoon/bronze/b3_2738/input.txt";
+const input = fs.readFileSync(filePath, "utf8").toString().trim().split("\n");
+const [n, m] = input[0].split(" ").map(Number);
+
+const result: number[][] = [];
+
+for (let i = 0; i < n; i++) {
+    const row1 = input[i + 1].split(" ").map(Number);
+    const row2 = input[i + n + 1].split(" ").map(Number);
+    result.push(row1.map((num, j) => num + row2[j]));
+}
+
+console.log(result.map(row => row.join(" ")).join("\n"));
