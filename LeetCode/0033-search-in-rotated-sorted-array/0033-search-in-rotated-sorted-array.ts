2489538cc574bfc6293c7dd4847f07e8da0234ce
이우성 <42796944+leewooseong@users.noreply.github.com>
2024-10-16 03:25:23 +0900

    Time: 79 ms (5.49%), Space: 53.8 MB (5.58%) - LeetHub

--- LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
+++ LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.ts
@@ -1,6 +1,7 @@
 // 무작정 찾아라 + 이분 탐색 문제라서 이분 탐색 적용 x
 // 정렬된 배열이라는 조건이 우선시 되어야한다! 까먹지 말자!
-
+// sort()의 경우 유니코드로 변환되어 2자리 숫자의 경우 첫번째 자리만 비교하는 문제가 있다.
+// 따라서, sort((a,b) => a - b)와 같이 두 수를 비교해서 정렬하도록 명시적으로 사용하는게 중요하다.
 function binarySearch(nums: number[], target: number): number {
   const len = nums.length;
   let start = 0;
@@ -35,7 +36,7 @@ function search(nums: number[], target: number): number {
   }
   const sortedNums = nums.slice().sort((a, b) => a - b);
   const sortedTargetIndex = binarySearch(sortedNums, target);
-  // console.log(sortedTargetIndex, pivotLen);
+  console.log(sortedNums, sortedTargetIndex, pivotLen);
 
   if (sortedTargetIndex === -1) {
     return -1;
@@ -48,3 +49,15 @@ function search(nums: number[], target: number): number {
     return targetIndex;
   }
 }
+
+// const nums = [4, 5, 6, 7, 0, 1, 2];
+// const target = 5;
+// const nums = [3, 1];
+// const target = 1;
+const nums = [15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14];
+const target = 25;
+
+const result = search(nums, target);
+console.log(result);
+
+export {};
